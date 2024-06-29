ded8685845fab2e459f25749715615ef99a3e304
ajcaesar <ajcaesar294@gmail.com>
2024-06-28 22:56:26 -0400

    built out app architecture

---
+++ job_tracker/index.js
@@ -0,0 +1,74 @@
+const addColButton = document.getElementById("add-section");
+const overlay = document.getElementById("overlay");
+const newColForm = document.getElementById("new-column");
+const newColDel = document.getElementById("new-col-del");
+const inputs = document.getElementById("all-inputs");
+let delButtons = document.querySelectorAll(".col-del-button")
+
+let colNames = JSON.parse(localStorage.getItem("colNames"));
+
+const removeCol = (event) => {
+    const targetElement = event.target.closest('.container');
+    const labelElement = targetElement.querySelector('.column-label');
+    const colName = labelElement.textContent;
+    colNames = colNames.filter(name => name !== colName);
+    localStorage.setItem("colNames", JSON.stringify(colNames));
+    renderNames();
+}
+
+const renderNames = () => {
+    let str = `<div class="container">
+        <label class="column-label" id="url" for="site-link">url</label>
+        <input class="column-input" type="text" id="site-link">
+    </div>`;
+    for (let col of colNames) {
+        str += `<div class="container">
+        <button class="del btn btn-block btn-danger col-del-button"><i class="fas fa-trash"></i></button>
+        <label class="column-label" for="${col}">${col}</label>
+        <input class="column-input" type="text" id="${col}">
+    </div>`;
+    }
+    inputs.innerHTML = str;
+    delButtons = document.querySelectorAll(".col-del-button");
+
+    delButtons.forEach(button => {
+        console.log("hello there");
+        button.addEventListener("click", removeCol);
+    });
+};
+
+if(!colNames) {
+    colNames = [];
+}
+else {
+    renderNames();
+}
+
+addColButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    newColForm.classList.toggle("hide");
+    overlay.classList.toggle("hide");
+});
+
+newColDel.addEventListener("click", (event) => {
+    event.preventDefault();
+    newColForm.classList.toggle("hide");
+    overlay.classList.toggle("hide");
+});
+
+newColForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const colNameInput = document.getElementById("new-column-input-text");
+
+    if (!colNameInput.value) {
+        return;
+    }
+    colNames.push(colNameInput.value);
+    localStorage.setItem("colNames", JSON.stringify(colNames));
+    colNameInput.value = "";
+    newColForm.classList.toggle("hide");
+    overlay.classList.toggle("hide");
+    renderNames();
+});
+
+
