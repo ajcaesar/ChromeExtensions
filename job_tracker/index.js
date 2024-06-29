fdb164320d8d8572bcd0d5f445537580e670b18b
ajcaesar <ajcaesar294@gmail.com>
2024-06-29 13:18:09 -0400

    local storage now stores variable values

--- job_tracker/index.js
+++ job_tracker/index.js
@@ -3,12 +3,24 @@ const overlay = document.getElementById("overlay");
 const newColForm = document.getElementById("new-column");
 const newColDel = document.getElementById("new-col-del");
 const inputs = document.getElementById("all-inputs");
-let delButtons = document.querySelectorAll(".col-del-button")
+let delButtons = document.querySelectorAll(".col-del-button");
 
-let colNames = JSON.parse(localStorage.getItem("colNames"));
+let colNames = JSON.parse(localStorage.getItem("colNames")) || [];
+let colInputs = JSON.parse(localStorage.getItem("colInputs")) || {};
 
 let urlInput = document.getElementById("site-link");
 const urlInputBtn = document.getElementById("input-url-in");
+
+function setInputs() {
+   colInputs = JSON.parse(localStorage.getItem("colInputs")) || {};
+   let allInputs =  document.querySelectorAll(".addedContainer");
+   for (let input of allInputs) {
+        let lab = colInputs[input.querySelector('.column-label').textContent];
+        if(lab) {
+            input.querySelector(".column-input").value = lab;
+        }
+   }
+}
 
 // Function to get and set the current tab's URL
 function setCurrentTabUrl() {
@@ -23,6 +35,57 @@ function setCurrentTabUrl() {
     } else {
         console.log("Chrome API not available. Are you testing in a browser?");
     }
+    renderNames();
+    setInputs();
+}
+
+// ... rest of your code
+const removeCol = (event) => {
+    const targetElement = event.target.closest('.container');
+    const labelElement = targetElement.querySelector('.column-label');
+    const colName = labelElement.textContent;
+    colNames = colNames.filter(name => name !== colName);
+    localStorage.setItem("colNames", JSON.stringify(colNames));
+    delete colInputs.colName;
+    localStorage.setItem("colInputs", JSON.stringify(colInputs));
+    renderNames();
+    setInputs();
+}
+
+let containers;
+const renderNames = () => {
+    let str = `<div class="container">
+        <label class="column-label" id="url" for="site-link">url</label>
+        <input class="column-input" type="text" id="site-link">
+    </div>`;
+    for (let col of colNames) {
+        str += `<div class="container addedContainer">
+        <button class="del btn btn-block btn-danger col-del-button"><i class="fas fa-trash"></i></button>
+        <label class="column-label" for="${col}">${col}</label>
+        <input class="column-input" type="text" id="${col}">
+    </div>`;
+    }
+    inputs.innerHTML = str;
+    containers = document.querySelectorAll(".addedContainer");
+    for (let container of containers) {
+        container.querySelector(".column-input").addEventListener("input", addInput);
+    }
+    delButtons = document.querySelectorAll(".col-del-button");
+    urlInput = document.getElementById("site-link");
+    delButtons.forEach(button => {
+        button.addEventListener("click", removeCol);
+    });
+    setInputs();
+};
+
+const addInput = (event) => {
+    let title = event.target.id;
+    colInputs[title] = event.target.value;
+    localStorage.setItem("colInputs", JSON.stringify(colInputs));
+}
+
+const saveToSpreadsheet = (event) => {
+    return;
 }
 
 // Set URL when the extension loads
@@ -30,46 +93,6 @@ document.addEventListener('DOMContentLoaded', setCurrentTabUrl);
 
 // Set URL when the button is clicked
 urlInputBtn.addEventListener('click', setCurrentTabUrl);
-
-// ... rest of your code
-
-const removeCol = (event) => {
-    const targetElement = event.target.closest('.container');
-    const labelElement = targetElement.querySelector('.column-label');
-    const colName = labelElement.textContent;
-    colNames = colNames.filter(name => name !== colName);
-    localStorage.setItem("colNames", JSON.stringify(colNames));
-    renderNames();
-}
-
-const renderNames = () => {
-    let str = `<div class="container">
-        <label class="column-label" id="url" for="site-link">url</label>
-        <input class="column-input" type="text" id="site-link">
-    </div>`;
-    for (let col of colNames) {
-        str += `<div class="container">
-        <button class="del btn btn-block btn-danger col-del-button"><i class="fas fa-trash"></i></button>
-        <label class="column-label" for="${col}">${col}</label>
-        <input class="column-input" type="text" id="${col}">
-    </div>`;
-    }
-    inputs.innerHTML = str;
-    delButtons = document.querySelectorAll(".col-del-button");
-    urlInput = document.getElementById("site-link");
-
-    delButtons.forEach(button => {
-        console.log("hello there");
-        button.addEventListener("click", removeCol);
-    });
-};
-
-if(!colNames) {
-    colNames = [];
-}
-else {
-    renderNames();
-}
 
 addColButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -97,5 +120,6 @@ newColForm.addEventListener("submit", (event) => {
     overlay.classList.toggle("hide");
     renderNames();
 });
+
 
 
